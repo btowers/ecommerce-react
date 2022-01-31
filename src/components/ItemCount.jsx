250ed877f04c7a1d36a02dc85411779c4eb6525a
brako2 <49849533+brako2@users.noreply.github.com>
2022-01-31 16:58:28 -0300

    clase 4

---
+++ src/components/ItemCount.jsx
@@ -0,0 +1,49 @@
+import React, { useState } from 'react';
+import { Button, Form } from 'react-bootstrap';
+
+const ItemCount = ({ stock, initial, onAdd }) => {
+  const [count, setCount] = useState(initial);
+
+  const handleAdd = () => {
+    if (count < stock) {
+      setCount(count + 1);
+    }
+  };
+
+  return (
+    <>
+      <Form className="m-3">
+        <Form.Group className="d-flex flex-row">
+          <Button
+            variant="primary"
+            size="sm"
+            className="smallButton"
+            onClick={handleAdd}
+          >
+            +
+          </Button>
+          <Form.Control
+            className="counter mx-2"
+            readOnly
+            size="sm"
+            type="email"
+            placeholder={count}
+          />
+          <Button
+            variant="primary"
+            size="sm"
+            className="smallButton"
+            onClick={handleAdd}
+          >
+            -
+          </Button>
+        </Form.Group>
+        <Button variant="primary" size="sm mt-2">
+          Agregar
+        </Button>
+      </Form>
+    </>
+  );
+};
+
+export default ItemCount;
--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -1,5 +1,6 @@
-import React from "react";
-import { Alert } from "react-bootstrap";
+import React from 'react';
+import { Alert } from 'react-bootstrap';
+import ItemCount from './ItemCount';
 
 const ItemListContainer = (prop) => {
   return (
@@ -8,6 +9,9 @@ const ItemListContainer = (prop) => {
         <Alert.Heading>¡Hola!</Alert.Heading>
         <p>{prop.greeting}</p>
       </Alert>
+      <ItemCount stock={10} initial={0} />
+      <ItemCount stock={5} initial={0} />
+      <ItemCount stock={30} initial={0} />
     </>
   );
 };
