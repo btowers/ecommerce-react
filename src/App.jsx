6dfae4ce28391bc680fb99e60d8d5f7c831a9bbd
Brian Towers <btowers@towers.ar>
2022-02-08 11:50:32 -0300

    clase 6

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,23 @@
-import React from 'react';
-import NavBar from './components/NavBar';
-import './App.css';
-import ItemListContainer from './components/ItemListContainer';
+import React from "react";
+import NavBar from "./components/NavBar";
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import ItemListContainer from "./components/ItemListContainer";
 
 function App() {
   return (
     <>
-      <NavBar />
-      <ItemListContainer greeting="Greeting enviado desde prop..." />
+      <Router>
+        <NavBar />
+        <Routes>
+          <Route path="/">
+            <ItemListContainer />
+          </Route>
+          <Route path="/item/:id">
+            <ItemListContainer />
+          </Route>
+        </Routes>
+      </Router>
     </>
   );
 }
--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -6,35 +6,38 @@ import {
   CardActions,
   CardContent,
   Typography,
+  Link,
 } from "@mui/material";
 
 const Item = ({ id, title, price, pictureUrl }) => {
   return (
     <>
       <Grid item key={id} xs={12} sm={6} md={4}>
-        <Card
-          sx={{
-            height: "100%",
-            display: "flex",
-            flexDirection: "column",
-          }}
-        >
-          <CardMedia
-            component="img"
-            sx={{ width: 200, height: 200 }}
-            image={pictureUrl}
-            alt={title}
-          />
-          <CardContent sx={{ flexGrow: 1 }}>
-            <Typography noWrap gutterBottom variant="h5" component="h2">
-              {title}
-            </Typography>
-            <Typography>${price}</Typography>
-          </CardContent>
-          <CardActions>
-            <Button size="small">Ver Detalles</Button>
-          </CardActions>
-        </Card>
+        <Link href={`/items/${id}`} key={id}>
+          <Card
+            sx={{
+              height: "100%",
+              display: "flex",
+              flexDirection: "column",
+            }}
+          >
+            <CardMedia
+              component="img"
+              sx={{ width: 200, height: 200 }}
+              image={pictureUrl}
+              alt={title}
+            />
+            <CardContent sx={{ flexGrow: 1 }}>
+              <Typography noWrap gutterBottom variant="h5" component="h2">
+                {title}
+              </Typography>
+              <Typography>${price}</Typography>
+            </CardContent>
+            <CardActions>
+              <Button size="small">Ver Detalles</Button>
+            </CardActions>
+          </Card>
+        </Link>
       </Grid>
     </>
   );
